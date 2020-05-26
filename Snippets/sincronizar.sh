e1732f637decaaf154cce02ec81e132f6ee39916
Eduard <eduard.duarte@hotmail.com>
2020-05-25 23:15:15 -0500

    actualiza snippets

--- Snippets/sincronizar.sh
+++ Snippets/sincronizar.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
-echo "Vamos a sincronizar snippets";
+
+# Funcion para subir cambios a github
+pushGH() {
+    mensaje="'""act snippets "$(date +'%d-%m-%Y %H:%M:%S')"'"
+    git pull o master
+    sleep 1
+    git add javascript.json
+    echo $mensaje
+    git commit -m $mensaje
+    git push o master
+}
+
+echo "Vamos a sincronizar snippets"
 dir=$(PWD)
-echo "directorio: " $dir 
+echo "directorio: " $dir
 # echo $LOCALAPPDATA
-echo $OneDrive;
+echo $OneDrive
 cd $OneDrive
 cd ../AppData/Roaming/Code/User/snippets
 ls
 cp javascript.json $dir/javascript.json
 
-if [$1 = "y"]
-then
-echo "entro al if"
+if [ $1 = "y" ]; then
+    echo "se van a subir los cambios al repositorio"
+    cd $dir
+    pushGH
+
 fi
 
-
+if [ $1 = 'p' ]; then
+    echo "Desea versionar en github, y - n"
+    read versionar
+    if [ $1 = 'y' ]; then
+        echo "se van a subir los cambios al repositorio"
+        cd $dir
+        pushGH
+    fi
+fi
